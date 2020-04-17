8f110646029906508806bd6435d79a1b551917d0
majkl zumberi <majklzumberi00@gmail.com>
2020-04-17 10:32:08 +0200

    added routing to all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { MemeListComponent } from './components/meme-list/meme-list.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { ProfiloComponent } from './components/profilo/profilo.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component:  HomepageComponent},
+  { path: 'memes', component:  MemeListComponent},
+  { path: 'feedback', component: FeedbackComponent},
+  { path: 'profilo', component: ProfiloComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ArrowGantiClique';
+  navLinks: any[];
+  activeLinkIndex = -1; 
+  constructor(private router: Router) {
+    this.navLinks = [
+        {
+            label: 'Homepage',
+            link: './home',
+            index: 0
+        }, {
+            label: 'memes',
+            link: './memes',
+            index: 1
+        }, {
+            label: 'FeedBack',
+            link: './feedback',
+            index: 2
+        }, 
+         {
+            label: 'Profilo',
+            link: './profilo',
+            index: 3
+        }, 
+    ];
+}
+ngOnInit(): void {
+  this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+  });
+}
 }
