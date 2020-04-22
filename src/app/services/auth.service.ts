8ddd605c7362210589609041da7dfe52115bf9a1
Federico Picone <federico.picone.develop@gmail.com>
2020-04-22 12:54:29 +0200

    Fix login component

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MemeListService } from './meme-list.service';
+import { Route } from '@angular/compiler/src/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +9,18 @@ import { MemeListService } from './meme-list.service';
 export class AuthService {
   users: any;
 
-  constructor(private http:MemeListService) { }
+  constructor(private http:MemeListService, private router:Router) { }
 
   signIn(username:string, password:string){
     this.http.getAllUsers().subscribe(users=>{
       users.forEach(user => {
         console.log(user);
+        if (username == user.username && password == user.password){
+          console.log("utente trovato, pronto per il login");
+          this.router.navigateByUrl("/home");
+        }
       });
-     
+      console.log("utente non trovato");
     })
   }
 }
