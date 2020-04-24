557de616610d99b54bfe3c0b1f1983114605c60f
Paulcait <57989682+Paulcait@users.noreply.github.com>
2020-04-24 14:47:14 +0200

    creazione detail component

---
+++ src/app/components/meme-detail/meme-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meme-detail',
+  templateUrl: './meme-detail.component.html',
+  styleUrls: ['./meme-detail.component.scss']
+})
+export class MemeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
