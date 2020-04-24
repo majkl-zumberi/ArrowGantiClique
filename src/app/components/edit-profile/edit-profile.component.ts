d3606f137fe74da56b797a635049b3fe740c5e9a
Federico Picone <federico.picone.develop@gmail.com>
2020-04-24 10:52:44 +0200

    Reactive form logic

--- src/app/components/edit-profile/edit-profile.component.ts
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { UserInterface } from 'src/app/models/UserInserface';
+import { MemeListService } from 'src/app/services/meme-list.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,10 +9,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.component.scss']
 })
 export class EditProfileComponent implements OnInit {
+  editForm: FormGroup;
 
-  constructor() { }
+
+  get nameControl():FormControl{
+    return this.editForm.get('editName') as FormControl;
+    }
+  
+  get surnameControl():FormControl{
+    return this.editForm.get('editSurname') as FormControl;
+    }
+
+  get genderControl():FormControl{
+    return this.editForm.get('editGender') as FormControl;
+    }
+
+  get emailControl():FormControl{
+    return this.editForm.get('editEmail') as FormControl;
+    }
+
+  get telControl():FormControl{
+    return this.editForm.get('editTel') as FormControl;
+    }
+
+
+  constructor(private fb:FormBuilder, private listService:MemeListService) {
+
+    this.editForm = this.fb.group({
+      editName:[""],
+      editSurname:[""],
+      editGender:[""],
+      editEmail:[""],
+      editTel:[""]
+    })
+
+   }
 
   ngOnInit(): void {
+  }
+
+  editUser(){
+    console.log("pronto per la submit della form");
+    console.log(this.nameControl.value);
+    console.log(this.surnameControl.value);
+    console.log(this.genderControl.value);
+    console.log(this.emailControl.value);
+    console.log(this.telControl.value);
+
+    let idUser = JSON.parse( sessionStorage.getItem('utente')).id;
+    let usernameUser = JSON.parse( sessionStorage.getItem('utente')).username;
+    let passwordUser = JSON.parse( sessionStorage.getItem('utente')).password;
+    sessionStorage.removeItem('utente');
+    let User:UserInterface = {
+      id:idUser,
+      nome:this.nameControl.value,
+      cognome:this.surnameControl.value,
+      sesso:this.genderControl.value,
+      email:this.emailControl.value,
+      telefono:this.telControl.value,
+      username:usernameUser,
+      password:passwordUser
+
+    }
+
+    sessionStorage.setItem('utente', JSON.stringify(User));
+    this.listService.updateUser(User, idUser).subscribe(user => {
+      console.log("utente aggiornato con successo zio");
+    })
+    
   }
 
 }
